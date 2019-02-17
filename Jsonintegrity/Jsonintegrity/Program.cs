443526136849082aa98c12138d2de65c280cb97f
AlyakseyHajieu <hajieualyaksey@gmail.com>
2019-02-18 00:31:59 +0300

    Update Program.cs
    first iteration

--- Jsonintegrity/Jsonintegrity/Program.cs
+++ Jsonintegrity/Jsonintegrity/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +8,44 @@ namespace Jsonintegrity
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string dirName = @ConfigurationManager.AppSettings["dirName"];
+        private static string outputFileName = @ConfigurationManager.AppSettings["outputFile"];
+
+        private static SortedDictionary<int, string> rawJsonCollection = new SortedDictionary<int, string>();
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string[] files = Directory.GetFiles(dirName);
+
+            foreach (string s in files)
+            {
+                JsonRawDataTile jsonRaw = new JsonRawDataTile(s);
+                if (jsonRaw.MessageTileNumber != 0)
+                {
+                    rawJsonCollection.Add(jsonRaw.MessageTileNumber, jsonRaw.JsonTextTile);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Метаданные файла {0} не валидны Код:0002.", s);
+                }
+
+            }
+
+            using (StreamWriter outputFile = new StreamWriter(outputFileName))
+            {
+                foreach (KeyValuePair<int, string> entry in rawJsonCollection)
+                {
+                    outputFile.WriteLine(entry.Value.Trim());
+                }
+            }
+
+            //string[] all = Directory.GetFiles(dirName, "*.txt")
+            //      .Select(x => Path.GetFileNameWithoutExtension(x))
+            //      .Distinct().ToArray();
+            Console.ReadLine();
         }
     }
 }
