5d23510571d83b7f3065d74704ec8c6372e984a5
AlyakseyHajieu <hajieualyaksey@gmail.com>
2019-02-18 00:44:04 +0300

    MetaDataParser added
    MetaDataParser added

--- Jsonintegrity/Jsonintegrity/ServiceBlock/JsonMetaDataParser/MetaDataParser.cs
+++ Jsonintegrity/Jsonintegrity/ServiceBlock/JsonMetaDataParser/MetaDataParser.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Jsonintegrity.ServiceBlock.JsonMetaDataParser
 {
     class MetaDataParser
     {
+        private static Regex jsonhashreg = new Regex(@ConfigurationManager.AppSettings["jsonhashreg"], RegexOptions.IgnoreCase);
+        private static Regex jsonNumberreg = new Regex(@ConfigurationManager.AppSettings["jsonNumberreg"], RegexOptions.IgnoreCase);
+
+        public static string GetJsonHash(string rawMetaData)
+        {
+            Match match = jsonhashreg.Match(rawMetaData);
+            string hash = match.Groups[1].Value;
+            return hash;
+        }
+
+        public static int GetJsonRawNumber(string rawMetaData)
+        {
+            int value = 0;
+            Match match = jsonNumberreg.Match(rawMetaData);
+            string number = match.Groups[1].Value;
+            try
+            {
+                value = int.Parse(number);
+            }
+            catch (FormatException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Для строки {0} не удалось высчитать порядковый номер Код:0001.", rawMetaData);
+            }
+            return value;
+        }
     }
 }
