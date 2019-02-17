dba4f3c0bddd51d29f13b76266df3c639c0b2c89
AlyakseyHajieu <hajieualyaksey@gmail.com>
2019-02-18 00:38:01 +0300

    add service classes
    add service classes

--- Jsonintegrity/Jsonintegrity/Program.cs
+++ Jsonintegrity/Jsonintegrity/Program.cs
@@ -1,8 +1,25 @@
-﻿using System;
+﻿using Jsonintegrity.ServiceBlock.JsonComponent;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+
+/*
+ * Настройки задаются через App.config
+ * -путь к папке с исходными данными json
+ * -путь по которому сохраняется созданный файл Json
+ * -регулярки для выборки хешЗначения , порядкового номера файла, разделителя метаДанных от основной информации
+ * 
+ * - планируемые доработки 
+ *      -- не учитывается блокировка, переход например на Threading 
+ *      --SortedDictionary довольно медленный подход - можно написать что то вроде - class ComparableRawJson<K, V> : IDictionary<K, V> where K: IComparable<K>
+ *      --отсутствует отвязка от конкретных реализация - как вариант интерфейсы и контенер(например ninject https://github.com/ninject/ninject)
+ *      --для файла конфигурации создать свой класс 
+ */
 
 namespace Jsonintegrity
 {
